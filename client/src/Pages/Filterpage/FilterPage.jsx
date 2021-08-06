9c9acb60927052fe4453e08fd3579581884cfb45
Dee-Hegde <dee.sirsi@gmail.com>
2021-08-06 20:49:15 +0530

    search added

---
+++ client/src/Pages/Filterpage/FilterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputSearch from '../../Components/Filterpage/InputSearch'
+
+function FilterPage() {
+    return (
+        <div>
+            <InputSearch/>
+        </div>
+    )
+}
+
+export default FilterPage
