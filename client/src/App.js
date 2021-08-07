9c01784043f9f8e7dc0c82e04884202938777ab3
KANDURI MANOHAR <manoharkanduri@gmail.com>
2021-08-07 17:50:45 +0530

    2forms completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { ParentsRegForm } from './Components/Forms/ParentsRegForm';
+import { SchoolRegForm } from './Components/Forms/SchoolRegForm';
 import Routes from './Routes/Routes';
 
 function App() {
   return (
     <div className="App">
      <Routes/>
+     <ParentsRegForm />
+     <SchoolRegForm/>
     </div>
   );
 }
